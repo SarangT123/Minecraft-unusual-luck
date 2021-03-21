cfa9e5622092da7638a474980a2b1574224fbd13
SarangT123 <72746858+SarangT123@users.noreply.github.com>
2021-03-21 20:25:23 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,287 @@
+def uiButtonClickEventListener1():
+    global run, e, ed, my_var, r, rd, tries
+    run = 0
+
+
+Button.onClickEvent('Button-stop', uiButtonClickEventListener1)
+
+
+def uiButtonClickEventListener2():
+    global run, e, ed, my_var, r, rd, tries
+    run = 1
+
+
+Button.onClickEvent('Button-start', uiButtonClickEventListener2)
+
+
+def uiButtonClickEventListener3():
+    global run, e, ed, my_var, r, rd, tries
+    run = 0
+    e = 0
+    ed = 0
+    my_var = 0
+    r = 0
+    rd = 0
+    run = 0
+    tries = 0
+    tries = 0
+
+
+Button.onClickEvent('Button-re', uiButtonClickEventListener3)
+
+
+def programStartListener1():
+    global run, e, ed, my_var, r, rd, tries
+    e = 0
+    ed = 0
+    my_var = 0
+    r = 0
+    rd = 0
+    run = 0
+    tries = 0
+    tries = 0
+    while True:
+        while run == 1:
+            e = random.randint(0, 10000)
+            r = random.randint(0, 10000)
+            if e < 473:
+                ed += 1
+                Label.setText('Label-e', ed)
+            if r < 5000:
+                rd += 1
+                Label.setText('Label-r', rd)
+            tries += 1
+            Label.setText('tries', tries)
+
+
+onProgramStart(programStartListener1)
+
+
+def programStartListener2():
+    global run, e, ed, my_var, r, rd, tries
+    while True:
+        while run == 1:
+            e = random.randint(0, 10000)
+            r = random.randint(0, 10000)
+            if e < 473:
+                ed += 1
+                Label.setText('Label-e', ed)
+            if r < 5000:
+                rd += 1
+                Label.setText('Label-r', rd)
+            tries += 1
+            Label.setText('tries', tries)
+
+
+onProgramStart(programStartListener2)
+
+
+def programStartListener3():
+    global run, e, ed, my_var, r, rd, tries
+    while True:
+        while run == 1:
+            e = random.randint(0, 10000)
+            r = random.randint(0, 10000)
+            if e < 473:
+                ed += 1
+                Label.setText('Label-e', ed)
+            if r < 5000:
+                rd += 1
+                Label.setText('Label-r', rd)
+            tries += 1
+            Label.setText('tries', tries)
+
+
+onProgramStart(programStartListener3)
+
+
+def programStartListener4():
+    global run, e, ed, my_var, r, rd, tries
+    while True:
+        while run == 1:
+            e = random.randint(0, 10000)
+            r = random.randint(0, 10000)
+            if e < 473:
+                ed += 1
+                Label.setText('Label-e', ed)
+            if r < 5000:
+                rd += 1
+                Label.setText('Label-r', rd)
+            tries += 1
+            Label.setText('tries', tries)
+
+
+onProgramStart(programStartListener4)
+
+
+def programStartListener5():
+    global run, e, ed, my_var, r, rd, tries
+    while True:
+        while run == 1:
+            e = random.randint(0, 10000)
+            r = random.randint(0, 10000)
+            if e < 473:
+                ed += 1
+                Label.setText('Label-e', ed)
+            if r < 5000:
+                rd += 1
+                Label.setText('Label-r', rd)
+            tries += 1
+            Label.setText('tries', tries)
+
+
+onProgramStart(programStartListener5)
+
+
+def programStartListener6():
+    global run, e, ed, my_var, r, rd, tries
+    while True:
+        while run == 1:
+            e = random.randint(0, 10000)
+            r = random.randint(0, 10000)
+            if e < 473:
+                ed += 1
+                Label.setText('Label-e', ed)
+            if r < 5000:
+                rd += 1
+                Label.setText('Label-r', rd)
+            tries += 1
+            Label.setText('tries', tries)
+
+
+onProgramStart(programStartListener6)
+
+
+def programStartListener7():
+    global run, e, ed, my_var, r, rd, tries
+    while True:
+        while run == 1:
+            e = random.randint(0, 10000)
+            r = random.randint(0, 10000)
+            if e < 473:
+                ed += 1
+                Label.setText('Label-e', ed)
+            if r < 5000:
+                rd += 1
+                Label.setText('Label-r', rd)
+            tries += 1
+            Label.setText('tries', tries)
+
+
+onProgramStart(programStartListener7)
+
+
+def programStartListener8():
+    global run, e, ed, my_var, r, rd, tries
+    while True:
+        while run == 1:
+            e = random.randint(0, 10000)
+            r = random.randint(0, 10000)
+            if e < 473:
+                ed += 1
+                Label.setText('Label-e', ed)
+            if r < 5000:
+                rd += 1
+                Label.setText('Label-r', rd)
+            tries += 1
+            Label.setText('tries', tries)
+
+
+onProgramStart(programStartListener8)
+
+
+def programStartListener9():
+    global run, e, ed, my_var, r, rd, tries
+    while True:
+        while run == 1:
+            e = random.randint(0, 10000)
+            r = random.randint(0, 10000)
+            if e < 473:
+                ed += 1
+                Label.setText('Label-e', ed)
+            if r < 5000:
+                rd += 1
+                Label.setText('Label-r', rd)
+            tries += 1
+            Label.setText('tries', tries)
+
+
+onProgramStart(programStartListener9)
+
+
+def programStartListener10():
+    global run, e, ed, my_var, r, rd, tries
+    while True:
+        while run == 1:
+            e = random.randint(0, 10000)
+            r = random.randint(0, 10000)
+            if e < 473:
+                ed += 1
+                Label.setText('Label-e', ed)
+            if r < 5000:
+                rd += 1
+                Label.setText('Label-r', rd)
+            tries += 1
+            Label.setText('tries', tries)
+
+
+onProgramStart(programStartListener10)
+
+
+def programStartListener11():
+    global run, e, ed, my_var, r, rd, tries
+    while True:
+        while run == 1:
+            e = random.randint(0, 10000)
+            r = random.randint(0, 10000)
+            if e < 473:
+                ed += 1
+                Label.setText('Label-e', ed)
+            if r < 5000:
+                rd += 1
+                Label.setText('Label-r', rd)
+            tries += 1
+            Label.setText('tries', tries)
+
+
+onProgramStart(programStartListener11)
+
+
+def programStartListener12():
+    global run, e, ed, my_var, r, rd, tries
+    while True:
+        while run == 1:
+            e = random.randint(0, 10000)
+            r = random.randint(0, 10000)
+            if e < 473:
+                ed += 1
+                Label.setText('Label-e', ed)
+            if r < 5000:
+                rd += 1
+                Label.setText('Label-r', rd)
+            tries += 1
+            Label.setText('tries', tries)
+
+
+onProgramStart(programStartListener12)
+
+
+def programStartListener13():
+    global run, e, ed, my_var, r, rd, tries
+    while True:
+        while run == 1:
+            e = random.randint(0, 10000)
+            r = random.randint(0, 10000)
+            if e < 473:
+                ed += 1
+                Label.setText('Label-e', ed)
+            if r < 5000:
+                rd += 1
+                Label.setText('Label-r', rd)
+            tries += 1
+            Label.setText('tries', tries)
+
+
+onProgramStart(programStartListener13)
+
+
